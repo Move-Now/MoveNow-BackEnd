d7c7ba5b60766028dc134a700d5e4bfdfd6da681
Lucas <lucas.txsan@gmail.com>
2023-06-23 21:16:37 -0300

    Conexão com o banco atualizada

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { routes } from "./routes/routes.js";
+import cors from 'cors'
 
 //instancia e configura o express
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 //utiliza rotas do arquivo routes.js
 app.use('/', routes)
--- src/database/database.js
+++ src/database/database.js
@@ -2,10 +2,10 @@ import mysql from "mysql2";
 
 //cria conexão com mysql
 export const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: "root",
-    password: "@23santosmysql2023",
-    database: "crud_node",
+    host: '108.167.132.214',
+    user: "gabr6451_user_movenow",
+    password: "pass_movenow",
+    database: "gabr6451_movenow",
     port: "3306",
 }).promise();
 
